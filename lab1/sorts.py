c424d3b7abd264097e8f10445ff22b23f04fdfee
Marcelo Basso <marcelo.basso@inf.ufrgs.br>
2024-04-01 13:28:25 -0300

    ⚡️ improve performance

--- lab1/sorts.py
+++ lab1/sorts.py
@@ -1,7 +1,7 @@
 import time
 import utils as u
 
-def insertion_sort(arr: list, h=1, tam=None) -> int:
+def insertion_sort(arr: list, h: int=1, tam: int=None):
     '''
     Insertion sort 
 
@@ -10,16 +10,14 @@ def insertion_sort(arr: list, h=1, tam=None) -> int:
         i: begining of array
         h: gap
         tam: length of array
-
-    Returns:
-        (int): number of moves made in the ordering
     '''
 
     # in case tam wasn't passed, calculates length
     if not tam:
         tam = len(arr)
 
-    moves = 0
+    key = 0
+    i = 0
     for j in range(h, tam):
         key = arr[j]
         i = j
@@ -27,13 +25,11 @@ def insertion_sort(arr: list, h=1, tam=None) -> int:
         while i >= h and arr[i - h] > key:
             arr[i] = arr[i - h]
             i -= h
-            moves += 1
 
         arr[i] = key
 
-    return moves
 
-def shell_sort(arr: list, tam: int, seq: list, name: str=None, out_f=None, time_f=None, cpu=None, verbose: bool=False) -> tuple:
+def shell_sort(arr: list, tam: int, seq: list, name: str=None, out_f=None, time_f=None, cpu=None, verbose: bool=False) -> float:
     '''
     Runs all the availabe sequences on shell_sort
         
@@ -48,12 +44,10 @@ def shell_sort(arr: list, tam: int, seq: list, name: str=None, out_f=None, time_
         verbose: verbose option to print information while executing
 
     Returns:
-        (moves, time): moves made and total execution time in milliseconds
+        time: total execution time in milliseconds
     '''
 
-    moves = 0
     seq_i = 0
-    i = 0
     arr_c = arr[:]
 
     if verbose:
@@ -67,7 +61,7 @@ def shell_sort(arr: list, tam: int, seq: list, name: str=None, out_f=None, time_
     runtime = time.process_time();
     while seq_i >= 0:
         h = seq[seq_i]
-        moves += insertion_sort(arr_c, h, tam)
+        insertion_sort(arr_c, h, tam)
         u.write_output(arr_c, f"INCR={h}", out_f) if verbose else 0
         seq_i -= 1 
     
@@ -77,4 +71,4 @@ def shell_sort(arr: list, tam: int, seq: list, name: str=None, out_f=None, time_
     if time_f:
         time_f.write(f"{name}, {tam}, {u.time_to_ms(runtime)}, {cpu}\n")
 
-    return (moves, u.time_to_ms(runtime))
+    return u.time_to_ms(runtime)
