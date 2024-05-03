e66ad8cb658ae7fdba196efec322107e883a1090
Marcelo Basso <bassomarcelo.ba@gmail.com>
2024-05-02 23:37:59 -0300

    Updates output files dir

--- lab3-radixsort/main.cpp
+++ lab3-radixsort/main.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main() {
     cout << "Processing sorted...\n";
-	sortArq("inputs/frankestein.txt", "outputs/frankestein_sorted.txt");
-	sortArq("inputs/war_and_peace.txt", "outputs/war_and_peace_sorted.txt");
+	sortArq("inputs/frankestein.txt", "frankestein_sorted.txt");
+	sortArq("inputs/war_and_peace.txt", "war_and_peace_sorted.txt");
 	
     cout << "Processing counted...\n";
-	sortCount("outputs/frankestein_sorted.txt", "outputs/frankestein_counted.txt");
-	sortCount("outputs/war_and_peace_sorted.txt", "outputs/war_and_peace_counted.txt");
+	sortCount("frankestein_sorted.txt", "frankestein_counted.txt");
+	sortCount("war_and_peace_sorted.txt", "war_and_peace_counted.txt");
 
     cout << "Processing ranked...\n";
-    sortFrq("outputs/frankestein_counted.txt", "outputs/frankestein_ranked.txt");
-    sortFrq("outputs/war_and_peace_counted.txt", "outputs/war_and_peace_ranked.txt");
+    sortFrq("frankestein_counted.txt", "frankestein_ranked.txt");
+    sortFrq("war_and_peace_counted.txt", "war_and_peace_ranked.txt");
 	
 	return 0;
 }
